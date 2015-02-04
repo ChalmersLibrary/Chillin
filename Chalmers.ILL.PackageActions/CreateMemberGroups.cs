0a3a9b23d8da4fe3ae4423df2c0b1ca56fc01885
Lars Andersson <lars.j.andersson@chalmers.se>
2015-02-04 11:32:23 +0100

    Added actions which creates member groups and type.

---
+++ Chalmers.ILL.PackageActions/CreateMemberGroups.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using umbraco.cms.businesslogic.packager.standardPackageActions;
+using umbraco.interfaces;
+using Umbraco.Core.Services;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using umbraco.cms.businesslogic.member;
+
+namespace Chalmers.ILL.PackageActions
+{
+    class CreateMemberGroups
+    {
+        public string Alias()
+        {
+            return "CreateMemberGroups";
+        }
+
+        public bool Execute(string packageName, System.Xml.XmlNode xmlData)
+        {
+            var ret = true;
+
+            try
+            {
+                MemberGroup.MakeNew("Administrator", umbraco.BusinessLogic.User.GetUser(0));
+                MemberGroup.MakeNew("Viewer", umbraco.BusinessLogic.User.GetUser(0));
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error<CreateMediaRootFolder>("Error in CreateMemberGroups.Execute: ", e);
+                ret = false;
+            }
+
+            return ret;
+        }
+
+        public System.Xml.XmlNode SampleXml()
+        {
+            var sample = "<Action runat=\"install\" undo=\"true\" alias=\"CreateMemberGroups\"></Action>";
+            return helper.parseStringToXmlNode(sample);
+        }
+
+        public bool Undo(string packageName, System.Xml.XmlNode xmlData)
+        {
+            var ret = true;
+
+            try
+            {
+                var mga = MemberType.GetByAlias("Administrator");
+                var mgv = MemberType.GetByAlias("Viewer");
+                mga.delete();
+                mgv.delete();
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error<CreateMediaRootFolder>("Error in CreateMemberGroups.Undo: ", e);
+                ret = false;
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ Chalmers.ILL.PackageActions/CreateMemberType.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using umbraco.cms.businesslogic.packager.standardPackageActions;
+using umbraco.interfaces;
+using Umbraco.Core.Services;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using umbraco.cms.businesslogic.member;
+
+namespace Chalmers.ILL.PackageActions
+{
+    class CreateMemberType : IPackageAction
+    {
+        public string Alias()
+        {
+            return "CreateMemberType";
+        }
+
+        public bool Execute(string packageName, System.Xml.XmlNode xmlData)
+        {
+            var ret = true;
+
+            try
+            {
+                MemberType.MakeNew(umbraco.BusinessLogic.User.GetUser(0), "Standard");
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error<CreateMediaRootFolder>("Error in CreateMemberType.Execute: ", e);
+                ret = false;
+            }
+
+            return ret;
+        }
+
+        public System.Xml.XmlNode SampleXml()
+        {
+            var sample = "<Action runat=\"install\" undo=\"true\" alias=\"CreateMemberType\"></Action>";
+            return helper.parseStringToXmlNode(sample);
+        }
+
+        public bool Undo(string packageName, System.Xml.XmlNode xmlData)
+        {
+            var ret = true;
+
+            try
+            {
+                var mt = MemberType.GetByAlias("Standard");
+                mt.delete();
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error<CreateMediaRootFolder>("Error in CreateMemberType.Undo: ", e);
+                ret = false;
+            }
+
+            return ret;
+        }
+    }
+}
