13e2a1f36840305401d9740a1afdf0c55cb4514d
Lars Andersson <lars.j.andersson@chalmers.se>
2015-02-11 13:25:38 +0100

    Added first time setup scripts.

---
+++ copy-umbraco-config-files.bat
@@ -0,0 +1,2 @@
+xcopy Chalmers.ILL\Web.config temp-backup\ /y
+xcopy packages\UmbracoCms.6.1.6\UmbracoFiles\Web.config Chalmers.ILL /y
---
+++ restore-chillin-config-files.bat
@@ -0,0 +1,2 @@
+xcopy temp-backup\Web.config Chalmers.ILL\ /y
+rd /S /Q temp-backup
