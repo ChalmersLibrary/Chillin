01fa57a3ce6e8038cfc99573bf3f0edbf487fea7
Lars Andersson <lars.j.andersson@chalmers.se>
2015-02-11 14:09:51 +0100

    Removed silly script files.

--- copy-umbraco-config-files.bat
+++
@@ -1,2 +0,0 @@
-xcopy Chalmers.ILL\Web.config temp-backup\ /y
-xcopy packages\UmbracoCms.6.1.6\UmbracoFiles\Web.config Chalmers.ILL /y
--- restore-chillin-config-files.bat
+++
@@ -1,2 +0,0 @@
-xcopy temp-backup\Web.config Chalmers.ILL\ /y
-rd /S /Q temp-backup
