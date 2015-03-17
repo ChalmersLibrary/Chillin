aeac58f0e5e5ba7a3d9dbaf9046965edf8478006
Lars Andersson <lars.j.andersson@chalmers.se>
2015-03-17 15:43:10 +0100

    More settings. Added tab for templates.

---
+++ Chalmers.ILL/Controllers/SurfaceControllers/TemplatesSurfaceController.cs
@@ -0,0 +1,24 @@
+﻿using Chalmers.ILL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Chalmers.ILL.Controllers.SurfaceControllers
+{
+    public class TemplatesSurfaceController : SurfaceController
+    {
+        public TemplatesSurfaceController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult RenderEditTemplatesAction()
+        {
+            return PartialView("Chalmers.ILL.EditTemplates", new TemplatesModel());
+        }
+    }
+}
