41efd0981f47408f388e45461ee6d4212605a19a
Lars Andersson <lars.j.andersson@chalmers.se>
2015-03-24 13:16:50 +0100

    Added possibility to change patron return date.

--- Chalmers.ILL/Controllers/SurfaceControllers/OrderItemPatronReturnDateSurfaceController.cs
+++ Chalmers.ILL/Controllers/SurfaceControllers/OrderItemPatronReturnDateSurfaceController.cs
@@ -1,6 +1,10 @@
-﻿using Chalmers.ILL.Models.PartialPage;
+﻿using Chalmers.ILL.Mail;
+using Chalmers.ILL.Models;
+using Chalmers.ILL.Models.Mail;
+using Chalmers.ILL.Models.PartialPage;
 using Chalmers.ILL.OrderItems;
 using Chalmers.ILL.Templates;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +19,13 @@ namespace Chalmers.ILL.Controllers.SurfaceControllers
     {
         IOrderItemManager _orderItemManager;
         ITemplateService _templateService;
+        IMailService _mailService;
 
-        public OrderItemPatronReturnDateSurfaceController(IOrderItemManager orderItemManager, ITemplateService templateService)
+        public OrderItemPatronReturnDateSurfaceController(IOrderItemManager orderItemManager, ITemplateService templateService, IMailService mailService)
         {
             _orderItemManager = orderItemManager;
             _templateService = templateService;
+            _mailService = mailService;
         }
 
         /// <summary>
@@ -36,6 +42,46 @@ namespace Chalmers.ILL.Controllers.SurfaceControllers
 
             // The return format depends on the client's Accept-header
             return PartialView("Chalmers.ILL.Action.PatronReturnDate", pageModel);
+        }
+
+        [HttpPost]
+        public ActionResult ChangeReturnDate(string packJson)
+        {
+            var json = new ResultResponse();
+
+            try
+            {
+                var pack = JsonConvert.DeserializeObject<ChangeReturnDatePackage>(packJson);
+
+                if (pack.logMsg != "")
+                {
+                    _orderItemManager.WriteLogItemInternal(pack.nodeId, "LOG", pack.logMsg, false, false);
+                }
+
+                _orderItemManager.SetDueDate(pack.nodeId, pack.dueDate, false, false);
+
+                _mailService.SendMail(pack.mail);
+                _orderItemManager.WriteLogItemInternal(pack.nodeId, "MAIL_NOTE", "Skickat mail till " + pack.mail.recipientEmail, false, false);
+                _orderItemManager.WriteLogItemInternal(pack.nodeId, "MAIL", pack.mail.message, true, true);
+
+                json.Success = true;
+                json.Message = "Återlämningsdatum mot låntagare ändrat.";
+            }
+            catch (Exception e)
+            {
+                json.Success = false;
+                json.Message = "Misslyckades med att ändra återlämningsdatum mot låntagare: " + e.Message;
+            }
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        public class ChangeReturnDatePackage
+        {
+            public int nodeId;
+            public string logMsg;
+            public DateTime dueDate;
+            public OutgoingMailModel mail;
         }
     }
 }
