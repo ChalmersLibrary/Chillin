1bc1cf11b1b0d12f2b44c5ab5f28ff108472e8f3
Lars Andersson <lars.j.andersson@chalmers.se>
2015-03-31 14:33:07 +0200

    Added new settings partial for modifying provider data.

---
+++ Chalmers.ILL/Controllers/SurfaceControllers/ProviderDataSurfaceController.cs
@@ -0,0 +1,29 @@
+﻿using Chalmers.ILL.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Chalmers.ILL.Controllers.SurfaceControllers
+{
+    [MemberAuthorize(AllowType = "Standard")]
+    public class ProviderDataSurfaceController : SurfaceController
+    {
+        ITemplateService _templateService;
+
+        public ProviderDataSurfaceController(ITemplateService templateService)
+        {
+            _templateService = templateService;
+        }
+
+        [HttpGet]
+        public ActionResult RenderModifyProviderDataAction()
+        {
+            var pageModel = new Models.PartialPage.Settings.ModifyProviderData();
+
+            return PartialView("Settings/ModifyProviderData", pageModel);
+        }
+    }
+}
---
+++ Chalmers.ILL/Models/PartialPage/Settings/ModifyProviderData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chalmers.ILL.Models.PartialPage.Settings
+{
+    public class ModifyProviderData
+    {
+    }
+}
