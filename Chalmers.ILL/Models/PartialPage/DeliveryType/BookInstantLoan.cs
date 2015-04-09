5c63fdc71708c612c9c3077e9a93ce08adcd5671
Lari Kovanen <lari.kovanen@chalmers.se>
2015-04-09 16:27:29 +0200

    Removed old bookslip template definitions

--- Chalmers.ILL/Models/PartialPage/DeliveryType/BookInstantLoan.cs
+++ Chalmers.ILL/Models/PartialPage/DeliveryType/BookInstantLoan.cs
@@ -8,7 +8,6 @@ namespace Chalmers.ILL.Models.PartialPage.DeliveryType
     public class BookInstantLoan : OrderItemPageModelBase
     {
         public string BookAvailableMailTemplate { get; set; }
-        public string BookSlipTemplate { get; set; }
 
         public BookInstantLoan(OrderItemModel orderItemModel) : base(orderItemModel) { }
     }
