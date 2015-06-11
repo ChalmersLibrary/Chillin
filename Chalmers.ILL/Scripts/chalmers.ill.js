1d5a0253d67ed9d1f148eeee88a5a08815c94d32
Lars Andersson <lars.j.andersson@chalmers.se>
2015-06-11 15:13:46 +0200

    SignalR 2.0.1 -> 2.0.3. Reconnect SignalR on disconnect.

--- Chalmers.ILL/Scripts/chalmers.ill.js
+++ Chalmers.ILL/Scripts/chalmers.ill.js
@@ -1155,6 +1155,12 @@ notifier.client.updateStream = function (value) {
     $("#debug-bucket .panel-body").prepend("<div>NodeId=" + value.NodeId + ", EditedBy=" + value.EditedBy + ", EditedByMemberName=" + value.EditedByMemberName + "</div>");
 };
 
+$.connection.hub.disconnected(function () {
+    setTimeout(function () {
+        $.connection.hub.start();
+    }, 5000); // Re-start connection after 5 seconds
+});
+
 /* Starting the signalR hub connections */
 $.connection.hub.start()
     .done(function () {
