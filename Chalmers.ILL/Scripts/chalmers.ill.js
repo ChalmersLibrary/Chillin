e12c79558fe6d0705c767b913b7d24b55f4683bd
Lars Andersson <lars.j.andersson@chalmers.se>
2015-06-11 14:59:22 +0200

    Reverting the two latest commits.

--- Chalmers.ILL/Scripts/chalmers.ill.js
+++ Chalmers.ILL/Scripts/chalmers.ill.js
@@ -1155,12 +1155,6 @@ notifier.client.updateStream = function (value) {
     $("#debug-bucket .panel-body").prepend("<div>NodeId=" + value.NodeId + ", EditedBy=" + value.EditedBy + ", EditedByMemberName=" + value.EditedByMemberName + "</div>");
 };
 
-$.connection.hub.disconnected(function () {
-    setTimeout(function () {
-        $.connection.hub.start();
-    }, 5000); // Re-start connection after 5 seconds
-});
-
 /* Starting the signalR hub connections */
 $.connection.hub.start()
     .done(function () {
@@ -1169,7 +1163,6 @@ $.connection.hub.start()
     .fail(function () {
         alert("Could not Connect to signalR notification hub");
     });
-
 
 /* Small helper functions */
 
