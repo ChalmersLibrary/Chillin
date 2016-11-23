daa4d7f9be9c49fe78cc3d15d9dc82ba44e6f509
Lars Andersson <lars.j.andersson@chalmers.se>
2016-11-23 12:12:14 +0100

    Renamed migration script.

--- Migration/hammer-api-method.pl
+++ Migration/hammer-api-method.pl
@@ -11,13 +11,14 @@ my $log_fh;
 # Auto flush the default buffer.
 $| = 1;
 
-if (scalar @ARGV eq 2) {
+if (scalar @ARGV eq 3) {
     my $now_string = strftime "%Y%m%d%H%M%S ", localtime;
     my $log_filename = "movelog$now_string.txt";
     open ($log_fh, '>', $log_filename) or die "Failed to open file '$log_filename' for writing.";
     
     my $url = $ARGV[0];
-    my $file = $ARGV[1];
+    my $paramName = $ARGV[1];
+    my $file = $ARGV[2];
     open(my $fh, '<:encoding(UTF-8)', $file) or die "Could not open file '$file' $!\n";
     
     print_everywhere("URL = $url\n");
@@ -28,7 +29,7 @@ if (scalar @ARGV eq 2) {
         chomp $nodeId;
         print_everywhere("Calling POST API method with value = $nodeId...   ");
         my $ua = LWP::UserAgent->new;
-        my $res = $ua->request(POST $url, [nodeId => $nodeId]);
+        my $res = $ua->request(POST $url, [$paramName => $nodeId]);
         if ($res->is_success) {
             my $res_data = JSON::XS->new->decode($res->content);
             if ($res_data->{ "Success" }) {
@@ -56,7 +57,7 @@ if (scalar @ARGV eq 2) {
 
 sub print_help {
     print "\n";
-    print "Usage: [SCRIPT FILENAME] [URL] [FILE]\n";
+    print "Usage: hammer-api-method.pl [URL] [PARAMETERNAME] [FILE]\n";
     print "Read values from specified file and call the POST API method at the given URL with each value.\n";
 }
 
