32661359f1c8dfff3843fa2d929645fd8b3e9d8d
Lars Andersson <lars.j.andersson@chalmers.se>
2017-05-04 13:47:32 +0200

    Added delivery type from provider.

---
+++ Chalmers.ILL/Models/PartialPage/DeliveryType/ArticleFromProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chalmers.ILL.Models.PartialPage.DeliveryType
+{
+    public class ArticleFromProvider : OrderItemPageModelBase
+    {
+        public ArticleFromProvider(OrderItemModel model) : base(model) { } 
+    }
+}
