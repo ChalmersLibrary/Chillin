bc4923fe270d6bc41456648e37e12ce87779a7a6
Lars Andersson <lars.j.andersson@chalmers.se>
2017-05-11 15:31:39 +0200

    Added logging to temporary silly controller.

--- Chalmers.ILL/Controllers/SurfaceControllers/TemporarySillySurfaceController.cs
+++ Chalmers.ILL/Controllers/SurfaceControllers/TemporarySillySurfaceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Umbraco.Core.Logging;
 using Umbraco.Web.Mvc;
 
 namespace Chalmers.ILL.Controllers.SurfaceControllers
@@ -20,12 +21,15 @@ namespace Chalmers.ILL.Controllers.SurfaceControllers
             {
                 try
                 {
+                    var childId = child.Id;
                     Services.ContentService.Delete(child);
                     count += 1;
+                    LogHelper.Debug<TemporarySillySurfaceController>("Removed child " + childId + ", total count = " + count);
                 }
                 catch (Exception e)
                 {
                     messages.Add(e.Message);
+                    LogHelper.Error<TemporarySillySurfaceController>("Error when removing child.", e);
                 }
             }
 
@@ -34,10 +38,12 @@ namespace Chalmers.ILL.Controllers.SurfaceControllers
                 var parent = Services.ContentService.GetById(parentId);
                 Services.ContentService.Delete(parent);
                 count += 1;
+                LogHelper.Debug<TemporarySillySurfaceController>("Removed parent " + parentId + ", total count = " + count);
             }
             catch (Exception e)
             {
                 messages.Add(e.Message);
+                LogHelper.Error<TemporarySillySurfaceController>("Error when removing parent.", e);
             }
 
             return Json(new { DeleteCount = count, Troubles = String.Join(" | ", messages) }, JsonRequestBehavior.AllowGet);
