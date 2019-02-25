a8079ce6c7f592f56ab9ca70dab642d6746f89dd
Jonas Johansson <jonas.johansson@chalmers.se>
2019-02-25 14:29:00 +0100

    Fixed mail templates

--- Chalmers.ILL/Controllers/SurfaceControllers/OrderItemDeliverySurfaceController.cs
+++ Chalmers.ILL/Controllers/SurfaceControllers/OrderItemDeliverySurfaceController.cs
@@ -405,9 +405,10 @@ namespace Chalmers.ILL.Controllers.SurfaceControllers
 
             // We save everything here first so that we get the new values injected into the message by the template service.
             _orderItemManager.SetPatronEmail(pack.nodeId, pack.mail.recipientEmail, eventId);
-
+           
             // Overwrite the message with message from template service so that we get the new values injected.
-            pack.mail.message = _templateService.GetTemplateData(mailTemplateName, _orderItemManager.GetOrderItem(pack.nodeId));
+            pack.mail.message = _templateService.ReplaceMoustaches("ArticleDeliveryByPostTemplate",
+                pack.mail.message, _orderItemManager.GetOrderItem(pack.nodeId));
 
             _mailService.SendMail(pack.mail);
             _orderItemManager.AddLogItem(pack.nodeId, "MAIL_NOTE", "Skickat mail till " + pack.mail.recipientEmail, eventId, false, false);
