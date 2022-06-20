26cbbea6d5abbe587c0fbc9586de432c8476c53e
larkov <lari.kovanen@chalmers.se>
2022-06-20 11:49:37 +0200

    Lotusfix for request

--- Chalmers.ILL/Models/CircualtionBasic.cs
+++ Chalmers.ILL/Models/CircualtionBasic.cs
@@ -7,11 +7,15 @@ namespace Chalmers.ILL.Models
         public string ItemId { get; set; }
         public string RequesterId { get; set; }
         public string RequestType { get; set; } = "Page";
+        public string RequestLevel { get; set; } = "Item";
         public string RequestDate { get; set; } = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ");
         public string FulfilmentPreference { get; set; } = "Hold Shelf";
         public string PickupServicePointId { get; set; }
+        public string InstanceId { get; set; }
+        public string HoldingsRecordId { get; set; }
 
-        public CirculationBasic(string itemId, string requesterId, string pickupServicePoint)
+       
+        public CirculationBasic(string itemId, string requesterId, string pickupServicePoint, string instanceId, string holdingId)
         {
             if (string.IsNullOrEmpty(itemId))
             {
@@ -25,9 +29,19 @@ namespace Chalmers.ILL.Models
             {
                 throw new ArgumentNullException(nameof(pickupServicePoint));
             }
+            if (string.IsNullOrEmpty(instanceId))
+            {
+                throw new ArgumentNullException(nameof(instanceId));
+            }
+            if (string.IsNullOrEmpty(holdingId))
+            {
+                throw new ArgumentNullException(nameof(holdingId));
+            }
             ItemId = itemId;
             RequesterId = requesterId;
             PickupServicePointId = pickupServicePoint;
+            InstanceId = instanceId;
+            HoldingsRecordId = holdingId;
         }
     }
 }
